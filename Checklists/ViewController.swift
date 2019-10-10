ec9aaa4f29319a1db8cd7405124f28a8a3cded38
Leo Chiriches <o.chiriches@outlook.com>
2019-10-10 16:49:31 +0300

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Leo Chiriches on 10/10/2019.
+//  Copyright © 2019 Leo Chiriches. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
